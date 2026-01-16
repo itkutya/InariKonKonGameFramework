7bf24808f50b4a32b2e18f89bca61fccb1d2f900
itkutya <itkutya@gmail.com>
2026-01-17 00:06:13 +0100

    AA

--- src/InariKonKon/Assets/Font/Font.cpp
+++ src/InariKonKon/Assets/Font/Font.cpp
@@ -1,6 +1,8 @@
 #include "InariKonKon/Assets/Font/Font.hpp"
 
 #include <algorithm>
+#include <cassert>
+#include <cstring>
 
 #include "ft2build.h" // IWYU pragma: keep
 #include FT_FREETYPE_H
@@ -14,17 +16,21 @@ namespace ikk
     Font::Font(const File& path, std::uint32_t fontSize) noexcept
         : m_fontSize(fontSize)
     {
+        //TODO:
+        /*
+        struct FTLibrary { FT_Library lib; ~FTLibrary(){ FT_Done_FreeType(lib); } };
+        struct FTFace { FT_Face face; ~FTFace(){ FT_Done_Face(face); } };
+        */
         FT_Library library;
-        if (FT_Init_FreeType(&library)) return;
+        if (FT_Init_FreeType(&library) != 0) return;
 
         FT_Face face;
-        if (FT_New_Face(library, path.getPath().string().c_str(), 0, &face)) { FT_Done_FreeType(library); return; }
-        if (FT_Set_Pixel_Sizes(face, 0, fontSize)) { FT_Done_Face(face); FT_Done_FreeType(library);return; }
+        if (FT_New_Face(library, path.getPath().string().c_str(), 0, &face) != 0) { FT_Done_FreeType(library); return; }
+        if (FT_Set_Pixel_Sizes(face, 0, fontSize) != 0) { FT_Done_Face(face); FT_Done_FreeType(library);return; }
 
         this->m_atlas.glyphs.reserve(face->num_glyphs);
 
         static constexpr std::uint32_t maxTextureWidth = 1024;
-        static constexpr std::uint32_t bytesPerPixel = 1u;
         static constexpr std::uint32_t padding = 5u;
 
         float penX = 0.0f;
@@ -37,7 +43,7 @@ namespace ikk
 
         while (glyph_index != 0)
         {
-            if (FT_Load_Glyph(face, glyph_index, FT_LOAD_DEFAULT | FT_LOAD_TARGET_NORMAL) == 0)
+            if (FT_Load_Glyph(face, glyph_index, FT_LOAD_DEFAULT) == 0)
             {
                 const FT_GlyphSlot& g = face->glyph;
 
@@ -70,7 +76,7 @@ namespace ikk
         this->m_atlas.width  = U32(std::max(maxRowWidth, penX));
         this->m_atlas.height = U32(penY + rowHeight);
 
-        this->m_atlas.texture.create(this->m_atlas.width, this->m_atlas.height, bytesPerPixel);
+        this->m_atlas.texture.create(this->m_atlas.width, this->m_atlas.height, 1);
         std::vector<std::byte>& buffer = this->m_atlas.texture.getBuffer();
 
         for (auto& [key, glyph] : this->m_atlas.glyphs)
@@ -80,13 +86,22 @@ namespace ikk
                 continue;
 
             const FT_Bitmap& bitmap = face->glyph->bitmap;
-            for (std::uint32_t y = 0; y < bitmap.rows; ++y)
-            {
-                const std::uint32_t dstY = U32(glyph.textureRect.getTop()) + y;
-                const std::uint32_t rowStart = (dstY * this->m_atlas.width + U32(glyph.textureRect.getLeft())) * bytesPerPixel;
 
-                for (std::uint32_t x = 0; x < bitmap.width; ++x)
-                    buffer[rowStart + x] = std::byte{ bitmap.buffer[y * bitmap.pitch + x] };
+            if (U32(glyph.textureRect.getLeft()) + bitmap.width > this->m_atlas.width ||
+                U32(glyph.textureRect.getTop()) + bitmap.rows > this->m_atlas.height)
+                return; //TODO:
+
+            const std::byte* basePtr = reinterpret_cast<const std::byte*>(bitmap.buffer);
+            if (bitmap.pitch < 0)
+                basePtr += (bitmap.rows - 1) * std::size_t(-bitmap.pitch);
+
+            for (std::size_t y = 0; y < bitmap.rows; ++y)
+            {
+                const std::size_t rowIndex = U32(glyph.textureRect.getTop()) + y;
+                const std::size_t rowStart = rowIndex * this->m_atlas.width + U32(glyph.textureRect.getLeft());
+                const std::byte* src = basePtr + std::ptrdiff_t(y) * std::abs(bitmap.pitch);
+
+                std::memcpy(buffer.data() + rowStart, src, bitmap.width);
             }
 
             glyph.textureRect.getLeft()   /= F32(this->m_atlas.width);
@@ -94,31 +109,6 @@ namespace ikk
             glyph.textureRect.getWidth()  /= F32(this->m_atlas.width);
             glyph.textureRect.getHeight() /= F32(this->m_atlas.height);
         }
-
-        /*
-        glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-
-        static GLuint tex = 0;
-        glGenTextures(1, &tex);
-        glBindTexture(GL_TEXTURE_2D, tex);
-
-        glTexImage2D(
-            GL_TEXTURE_2D,
-            0,
-            GL_R8,
-            m_atlas.width,
-            m_atlas.height,
-            0,
-            GL_RED,
-            GL_UNSIGNED_BYTE,
-            buffer.data()
-        );
-
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-        */
 
         FT_Done_Face(face);
         FT_Done_FreeType(library);
